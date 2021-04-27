da6f844ab796b3f905bb47809608e462234d73ed
shahad <shahad@test>
2021-04-28 00:32:04 +0300

    initial commit

---
+++ WinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Line = new System.Windows.Forms.Button();
+            this.Circle = new System.Windows.Forms.Button();
+            this.Rectangle = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel1.Controls.Add(this.Line);
+            this.panel1.Controls.Add(this.Circle);
+            this.panel1.Controls.Add(this.Rectangle);
+            this.panel1.Location = new System.Drawing.Point(2, 447);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(855, 115);
+            this.panel1.TabIndex = 0;
+            // 
+            // Line
+            // 
+            this.Line.Location = new System.Drawing.Point(543, 23);
+            this.Line.Name = "Line";
+            this.Line.Size = new System.Drawing.Size(112, 62);
+            this.Line.TabIndex = 0;
+            this.Line.Text = "Line";
+            this.Line.UseVisualStyleBackColor = true;
+            this.Line.Click += new System.EventHandler(this.Line_Click);
+            // 
+            // Circle
+            // 
+            this.Circle.Location = new System.Drawing.Point(264, 23);
+            this.Circle.Name = "Circle";
+            this.Circle.Size = new System.Drawing.Size(112, 62);
+            this.Circle.TabIndex = 2;
+            this.Circle.Text = "Circle";
+            this.Circle.UseVisualStyleBackColor = true;
+            this.Circle.Click += new System.EventHandler(this.Circle_Click);
+            // 
+            // Rectangle
+            // 
+            this.Rectangle.Location = new System.Drawing.Point(404, 23);
+            this.Rectangle.Name = "Rectangle";
+            this.Rectangle.Size = new System.Drawing.Size(112, 62);
+            this.Rectangle.TabIndex = 1;
+            this.Rectangle.Text = "Rectangle";
+            this.Rectangle.UseVisualStyleBackColor = true;
+            this.Rectangle.Click += new System.EventHandler(this.buttonl2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(38, 52);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 52);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Color";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.radioButton5);
+            this.panel2.Controls.Add(this.radioButton6);
+            this.panel2.Controls.Add(this.radioButton3);
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(-3, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(169, 451);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Style :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(15, 203);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(77, 29);
+            this.radioButton5.TabIndex = 2;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Dash";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(15, 238);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(93, 29);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Dotted";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(15, 390);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(47, 29);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 355);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 29);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 320);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 29);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Size:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(850, 558);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paint App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Circle;
+        private System.Windows.Forms.Button Rectangle;
+        private System.Windows.Forms.Button Line;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+    }
+}
+
---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        Pen p;
+        public int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+        public int shapeID;
+        Graphics mg;
+        public bool drag;
+        public Form1()
+        {
+            InitializeComponent();
+            shapeID = 0;
+            mg = this.CreateGraphics();
+            p = new Pen(Color.Black);
+            drag = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonl2_Click(object sender, EventArgs e)
+        {
+            shapeID = 4;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult;
+            dialogResult = colorDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                p.Color = colorDialog1.Color;
+
+            }
+        }
+        private void Line_Click(object sender, EventArgs e)
+        {
+            shapeID = 3;
+        }
+
+        private void Circle_Click(object sender, EventArgs e)
+        {
+            shapeID = 2;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (shapeID)
+            {
+                case 2: mg.DrawEllipse(p, x1, y1, x2 - x1, y2 - y1);
+                        break;
+                case 3: mg.DrawLine(p, x1, y1, x2, y2);
+                        break;
+                case 4: mg.DrawRectangle(p, x1, y1, x2 - x1, y2 - y1);
+                        break;
+            }
+    
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x2 = e.X;
+            y2 = e.Y;
+
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked) p.Width = 5;
+            else if (radioButton2.Checked) p.Width = 10;
+            else if (radioButton3.Checked) p.Width = 15;
+            if(radioButton5.Checked) p.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+            
+
+        }
+
+       
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x1 = e.X;
+            y1 = e.Y;
+        }
+    }
+}
